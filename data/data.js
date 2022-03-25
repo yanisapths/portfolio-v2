d048f0d9269bf18e7850f1ab4f81ac5349ffbc76
yanisapths <72002605+yanisapths@users.noreply.github.com>
2022-03-25 09:09:29 +0700

    about 99%

--- data/data.js
+++ data/data.js
@@ -17,6 +17,12 @@ const data = {
         linkedin: "https://www.linkedin.com/in/yanisa-poongthaisong-148629125/",
         medium: "	https://medium.com/@yanisa-poongthaisong",
         github: "https://github.com/yanisapths",
+    },
+    cert: {
+        huawei: "/asset/huawei.pdf",
+        network: "https://www.udemy.com/certificate/UC-621cd0cd-c42c-4dc6-b635-272fec4aac97/",
+        ether: "https://www.udemy.com/certificate/UC-50a839aa-d0e4-41af-893e-fc11ea6269e5/",
+        cisco: "/asset/cisco.pdf",
     }
 }
 
