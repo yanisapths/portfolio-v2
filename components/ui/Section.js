60333db07bac1094c9b764c3d2820df044c1cf29
yanisapths <72002605+yanisapths@users.noreply.github.com>
2022-03-26 22:47:06 +0700

    fix img md size

--- components/ui/Section.js
+++ components/ui/Section.js
@@ -4,7 +4,7 @@ import Link from "next/link"
 function Section() {
   return (
     <section>
-        <div className="px-8 py-16 mx-auto max-w-screen-2xl sm:px-6 lg:px-60 xl:px-96 xl:mt-10 xl:mb-40" >
+        <div className="px-8 py-16 mx-auto max-w-screen-2xl sm:px-6 lg:px-60 xl:px-96 xl:mt-10 xl:mb-40 md:px-48" >
             <div className="grid grid-cols-1 lg:grid-cols-2 h:-1/6 lg:h-1/6 xl:h-[10rem] ">
             <div className="relative z-10 lg:py-12 md:px-2 lg:px-2 xl:px-2 xl:py-6 px-20">
                 <div className="relative h-40 sm:h-80 lg:h-full shadow-2xl shadow-indigo-700">
@@ -29,7 +29,7 @@ function Section() {
                 <p className="mt-4 text-gray-600 text-lg pr-4">
                     Read general info, some past experience, and my background on this page.
                 </p>
-                <div   className="inline-block font-bold px-12 py-3 mt-8 text-md text-indigo-200 bg-[#070738] border border-indigo-400 rounded-lg active:text-indigo-400 hover:bg-white hover:text-indigo-400 focus:outline-none focus:ring shadow-indigo-400 shadow-md">
+                <div   className="cursor-pointer inline-block font-bold px-12 py-3 mt-8 text-md text-indigo-200 bg-[#070738] border border-indigo-400 rounded-lg active:text-indigo-400 hover:bg-white hover:text-indigo-400 focus:outline-none focus:ring shadow-indigo-400 shadow-md">
                 <Link
                     href="/about"
                 >
